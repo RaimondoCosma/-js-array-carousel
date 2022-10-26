8637c5aee4066fd0ec2bf66fbea02b767538fd31
RaimondoCosma <raimondo.cosma@gmail.com>
2022-10-26 17:27:55 +0200

    Cambio metodo per aggiungere le immagini al DOM

--- js/my-script.js
+++ js/my-script.js
@@ -10,8 +10,20 @@ const carouselImg = [
 
 // 2. Creo un ciclo for per inserire dinamicamente le immagini
 const imgContainer = document.querySelector('.items');
-for ( let i = 0; i < carouselImg.length; i++ ) {
-    let itemContent = `<div class="item"><img src="${carouselImg[i]}"></div>`;
-    imgContainer.insertAdjacentHTML('beforeend', itemContent);
-};
+// for ( let i = 0; i < carouselImg.length; i++ ) {
+//     let itemContent = `<div class="item"><img src="${carouselImg[i]}"></div>`;
+//     imgContainer.insertAdjacentHTML('beforeend', itemContent);
+// };
 
+// Provo un altro metodo. Problemi con aggiungere altre classi al template literal
+for (let i = 0; i < carouselImg.length; i++) {
+    const item = document.createElement("div");
+    item.classList.add("item");
+    if(i === 0) {
+        item.classList.add("active");
+    }
+    const img = document.createElement("img");
+    img.setAttribute("src", carouselImg[i]);
+    item.append(img);
+    imgContainer.append(item);
+}
